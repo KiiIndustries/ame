47a54b98298c35c89f5deaae1719d7c17a7f315d
Kathrine Lemet <kiiindustries@protonmail.com>
2022-01-31 09:02:42 -0700

    Added the canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+// First let's create the Canvas and Context the user will
+// interact with
+const Canvas  = document.createElement("CANVAS");
+Canvas.width  = 100;
+Canvas.Height = 100;
+const Context = Canvas.getContext("2d");
+// Then we can go ahead and add them to the document.
+document.body.appendChild(Canvas);
+// Now let's see if it's there
+Context.fillRect(10,10,80,80);
